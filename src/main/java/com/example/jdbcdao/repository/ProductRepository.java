a6a1b9f62b62427d34c8123700d0b84b08f4be25
Andrey Zemlyachev <kvintilian@gmail.com>
2021-01-26 23:26:19 +0300

    Update ProductRepository.java

--- src/main/java/com/example/jdbcdao/repository/ProductRepository.java
+++ src/main/java/com/example/jdbcdao/repository/ProductRepository.java
@@ -1,18 +1,24 @@
 package com.example.jdbcdao.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.stream.Collectors;
+
 
 @Repository
 public class ProductRepository {
+
+  private final String SCRIPT_NAME = "select_order_by_customer.sql";
 
   private final String query;
 
@@ -20,7 +26,7 @@ public class ProductRepository {
   private NamedParameterJdbcTemplate jdbcTemplate;
 
   public ProductRepository() {
-    this.query = read("select_order_by_customer.sql");
+    this.query = read(SCRIPT_NAME);
   }
 
   public String getProductName(String name) {
@@ -33,19 +39,17 @@ public class ProductRepository {
     return stringBuilder.toString();
   }
 
-  public String read(String fileName) {
-    StringBuilder result = new StringBuilder();
-    ClassLoader classLoader = getClass().getClassLoader();
-    File file = new File(Objects.requireNonNull(classLoader.getResource(fileName)).getFile());
-    try (Scanner scanner = new Scanner(file)) {
-      while (scanner.hasNextLine()) {
-        String line = scanner.nextLine();
-        result.append(line).append("\n");
-      }
+  public static String read(String scriptName) {
+    try (InputStream is = new ClassPathResource(scriptName).getInputStream()) {
+      return String.join("\n", readLines(is));
     } catch (IOException e) {
-      e.printStackTrace();
+      throw new RuntimeException(e);
     }
-    return result.toString();
+  }
+
+  private static List<String> readLines(InputStream is) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+    return reader.lines().collect(Collectors.toList());
   }
 
 }
